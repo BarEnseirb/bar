b69c97d97e49e776e5457cebf5f176b3f495b08f
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-09-09 11:34:16 +0200

    Added lock and start sqlite db

---
+++ src/main/java/fr/pjdevs/bar/Accounts.java
@@ -0,0 +1,31 @@
+package fr.pjdevs.bar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Accounts {
+    /**
+     * Demo method
+     */
+    public void getAccountList() {
+        try (Connection conn = DriverManager.getConnection(
+            "jdbc:sqlite:" + System.getProperty("user.dir") + "\\data\\db\\bar.db",
+            "",
+            "")) {
+            
+            try (Statement stmt = conn.createStatement()) {
+                ResultSet r = stmt.executeQuery("SELECT * from student");
+
+                while (r.next()) {
+                    System.out.println(r.getString("NAME"));
+                }
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
--- src/main/java/fr/pjdevs/bar/App.java
+++ src/main/java/fr/pjdevs/bar/App.java
@@ -1,9 +1,16 @@
 package fr.pjdevs.bar;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.Exception;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.StandardOpenOption;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
@@ -13,8 +20,36 @@ import javafx.fxml.FXMLLoader;
  * The main layout {@code App.fxml} loaded here and the window is created.
  */
 public class App extends Application {
+    /**
+     * Try to acquire a lock to avoid multiple instance running simultaneously.
+     */
+    private boolean acquireLock() {
+        File file = new File(System.getProperty("user.dir"), "bar.lock");
+
+        try {
+            FileChannel fc = FileChannel.open(file.toPath(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.WRITE);
+            FileLock lock = fc.tryLock();
+            
+            return lock != null;
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
+        // Try to acquire lock
+        try {
+            if (!acquireLock()) {
+                new Alert(AlertType.ERROR, "An instance is already running.").show();
+            }
+        } catch(Error e) {
+            new Alert(AlertType.ERROR, e.toString()).show();
+        }
+
+        // Create window
         final Parent root = FXMLLoader.load(getClass().getResource("fxml/App.fxml"));
         root.getStylesheets().add(getClass().getResource("css/bar.css").toExternalForm());
         final Scene scene = new Scene(root, 600, 800);
