04118a5067c1cc12697ce44442996a262f8f7d82
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-09-21 09:58:07 +0200

    reverse history

--- src/main/java/fr/pjdevs/bar/Account.java
+++ src/main/java/fr/pjdevs/bar/Account.java
@@ -4,23 +4,17 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class Account {
-    private int id;
     public SimpleStringProperty login;
     public SimpleStringProperty name;
     public SimpleIntegerProperty money;
     public SimpleIntegerProperty year;
     public SimpleIntegerProperty sector;
 
-    public Account(int id, String login, String name, int money, int year, int sector) {
-        this.id = id;
+    public Account(String login, String name, int money, int year, int sector) {
         this.login = new SimpleStringProperty(login);
         this.name = new SimpleStringProperty(name);
         this.money = new SimpleIntegerProperty(money);
         this.year = new SimpleIntegerProperty(year);
         this.sector = new SimpleIntegerProperty(sector);
-    }
-
-    public int getId() {
-        return this.id;
     }
 }
--- src/main/java/fr/pjdevs/bar/DatabaseConnection.java
+++ src/main/java/fr/pjdevs/bar/DatabaseConnection.java
@@ -18,7 +18,7 @@ public class DatabaseConnection implements AutoCloseable {
     public Account getAccount(String login) throws SQLException {
         ResultSet r = this.connection.createStatement().executeQuery("select * from student where login = '" + login + "'");
 
-        return new Account(r.getInt("oid"), r.getString("login"), r.getString("name"), r.getInt("money"), r.getInt("year"), r.getInt("class"));
+        return new Account(r.getString("login"), r.getString("name"), r.getInt("money"), r.getInt("year"), r.getInt("class"));
     }
 
     public List<Account> getAccountList() throws SQLException {
@@ -26,7 +26,7 @@ public class DatabaseConnection implements AutoCloseable {
         List<Account> accounts = new ArrayList<Account>();
 
         while (r.next()) {
-            accounts.add(new Account(r.getInt("oid"), r.getString("login"), r.getString("name"), r.getInt("money"), r.getInt("year"), r.getInt("class")));
+            accounts.add(new Account(r.getString("login"), r.getString("name"), r.getInt("money"), r.getInt("year"), r.getInt("class")));
         }
 
         return accounts;
