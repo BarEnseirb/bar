471f59f5d599249f1c5ee1e93e23aaf201e83f65
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-09-23 08:46:08 +0200

    unused import

--- src/main/java/fr/pjdevs/bar/AccountTab.java
+++ src/main/java/fr/pjdevs/bar/AccountTab.java
@@ -11,7 +11,6 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tab;
