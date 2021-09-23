e3b0d132f3118f817fbcfc5e40535d92413663a2
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-09-23 10:26:45 +0200

    Prepare new interface

--- src/main/java/fr/pjdevs/bar/CartPane.java
+++ src/main/java/fr/pjdevs/bar/CartPane.java
@@ -7,13 +7,12 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
 
-public class CartPane extends BorderPane {
+public class CartPane extends VBox {
     private int total;
 
     public CartPane() {
