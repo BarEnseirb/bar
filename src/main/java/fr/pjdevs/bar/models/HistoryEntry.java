d0e09f4c9a74ab63295500c65a353c48ce2801e5
Pierre-Jean MOREL <pj33720@gmail.com>
2021-10-11 13:56:59 +0200

    HistoryEntry doc

--- src/main/java/fr/pjdevs/bar/models/HistoryEntry.java
+++ src/main/java/fr/pjdevs/bar/models/HistoryEntry.java
@@ -10,13 +10,39 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+/**
+ * Class that represents an history entry in the database.
+ */
 public class HistoryEntry {
+    /**
+     * Login of the student concerned by the entry.
+     */
     private StringProperty studentLogin;
+    /**
+     * The product name of the entry.
+     */
     private StringProperty product;
+    /**
+     * The price of the entry.
+     */
     private IntegerProperty price;
+    /**
+     * The date of the entry.
+     */
     private ObjectProperty<Date> date;
+    /**
+     * The transaction type of the entry.
+     */
     private StringProperty transaction;
 
+    /**
+     * Creates a new HistoryEntry instance.
+     * @param login Login of the student concerned by the entry.
+     * @param product The product name of the entry.
+     * @param price The price of the entry.
+     * @param date The date of the entry.
+     * @param transaction The transaction type of the entry.
+     */
     public HistoryEntry(String login, String product, int price, Date date, String transaction) {
         this.studentLogin = new SimpleStringProperty(login);
         this.product = new SimpleStringProperty(product);
@@ -26,42 +52,72 @@ public class HistoryEntry {
         this.transaction = new SimpleStringProperty(transaction);
     }
 
+    /**
+     * Gets the login of the student concerned by this entry.
+     */
     public String getStudentLogin() {
         return this.studentLogin.get();
     }
 
+    /**
+     * The login of the student concerned by this entry.
+     */
     public StringProperty studentLoginProperty() {
         return this.studentLogin;
     }
 
+    /**
+     * Gets the product name of this entry.
+     */
     public String getProduct() {
         return this.product.get();
     }
 
+    /**
+     * The product name of this entry.
+     */
     public StringProperty productProperty() {
         return this.product;
     }
 
+    /**
+     * Gets the price of this entry.
+     */
     public int getPrice() {
         return this.price.get();
     }
 
+    /**
+     * The price of this entry.
+     */
     public IntegerProperty priceProperty() {
         return this.price;
     }
 
+    /**
+     * Gets the date of this entry.
+     */
     public Date getDate() {
         return this.date.get();
     }
 
+    /**
+     * The date of this entry.
+     */
     public ObjectProperty<Date> dateProperty() {
         return this.date;
     }
 
+    /**
+     * Gets the transaction type of this entry.
+     */
     public String getTransaction() {
         return this.transaction.get();
     }
 
+    /**
+     * The transaction type of this entry.
+     */
     public StringProperty transactionProperty() {
         return this.transaction;
     }
