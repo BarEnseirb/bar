c4a65563aaf9f9a5d61eccbfd609c47fdbbfa693
Pierre-Jean MOREL <pj33720@gmail.com>
2021-10-11 15:04:10 +0200

    ItemListTest doc

--- src/test/java/fr/pjdevs/bar/ItemListTest.java
+++ src/test/java/fr/pjdevs/bar/ItemListTest.java
@@ -6,23 +6,33 @@ import fr.pjdevs.bar.models.ItemList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for {@link ItemList}.
+ */
 class ItemListTest {
 
+    /**
+     * Tests if the singleton instance itself is not {@code null}.
+     * @throws Exception
+     */
     @Test
     void itemListInstanceNotNull() throws Exception {
         assertNotEquals(ItemList.getInstance(), null);
     }
 
+    /**
+     * Tests if the list of the singleton instance is not {@code null}.
+     * @throws Exception
+     */
     @Test
     void itemListNotNull() throws Exception {
         assertNotEquals(ItemList.getInstance().getList(), null);
     }
 
-    @Test
-    void itemListNotEmpty() throws Exception {
-        assertTrue(ItemList.getInstance().getList().size() > 0);
-    }
-
+    /**
+     * Tests if the list of the singleton instance is not empty.
+     * @throws Exception
+     */
     @Test
     void itemListItemsNotNull() throws Exception {
         assertNotEquals(ItemList.getInstance().getList().get(0), null);
