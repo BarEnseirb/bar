7b39e6ed0ff1976bd83c33e6ae0b471372eae9f5
Pierre-Jean MOREL <pj33720@gmail.com>
2021-10-11 14:37:54 +0200

    IntegerStringMoneyCallback doc

--- src/main/java/fr/pjdevs/bar/util/IntegerStringMoneyCallback.java
+++ src/main/java/fr/pjdevs/bar/util/IntegerStringMoneyCallback.java
@@ -1,26 +1,31 @@
 package fr.pjdevs.bar.util;
 
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.util.Callback;
 
+/**
+ * Callback for the {@link CellValueFactory} to convert an {@link Integer} representing money to decimal {@link String}.
+ * @param <T> Type of the model which contains the {@link IntegerProperty}.
+ */
 public class IntegerStringMoneyCallback<T> implements Callback<CellDataFeatures<T, String>, ObservableValue<String>> {
-
+    /**
+     * The callback to retrieve the {@link IntegerProperty}.
+     */
     private Callback<T, IntegerProperty> getIntegerProperty;
 
+    /**
+     * Creates a new IntegerStringMoneyCallback instance.
+     * @param getIntegerProperty The callback to call on {@link T} to get its {@link IntegerProperty}.
+     */
     public IntegerStringMoneyCallback(Callback<T, IntegerProperty> getIntegerProperty) {
         this.getIntegerProperty = getIntegerProperty;
     }
 
     @Override
     public ObservableValue<String> call(CellDataFeatures<T, String> cellData) {
-        return Bindings.createStringBinding(() -> 
-            String.format("%.2fE",
-                getIntegerProperty.call(cellData.getValue()).get()/100.0),
-                getIntegerProperty.call(cellData.getValue())
-        );
+        return MoneyStringBindings.createIntegerMoneyStringBinding(getIntegerProperty.call(cellData.getValue()));
     }
     
 }
