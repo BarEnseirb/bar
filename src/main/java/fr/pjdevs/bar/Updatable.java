07ba50f7b5920b71982cdf4c52d3f4fff6f46c5d
Pierre-Jean MOREL <pj33720@gmail.com>
2021-10-11 11:48:57 +0200

    Updatable doc

--- src/main/java/fr/pjdevs/bar/Updatable.java
+++ src/main/java/fr/pjdevs/bar/Updatable.java
@@ -2,7 +2,13 @@ package fr.pjdevs.bar;
 
 import javafx.fxml.FXML;
 
+/**
+ * Interface which allows a component to be updatable.
+ */
 public interface Updatable {
+    /**
+     * FXML accessible method which updates this component.
+     */
     @FXML
     public void update(); 
 }
